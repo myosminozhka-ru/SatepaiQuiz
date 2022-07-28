9f737a734e723c68f6998b1715070b3161aae4b3
Sergey Goncharovр <doimpos@gmail.com>
2022-07-29 00:56:19 +0500

    Доработки

--- src/blocks/modules/content/content.js
+++ src/blocks/modules/content/content.js
@@ -1,13 +1,13 @@
 const MainContent = class MainContent {
     constructor() {}
     chooseScreen(target) {
-        if (!document.querySelector(`.header__mobile_menu-in.isActive [data-target="${target}"]`).dataset.back) {
+        if (!document.querySelector(`.content__in.isActive [data-target="${target}"]`).dataset.back) {
             console.log(`вперед к ${target}`);
-            document.querySelector('.header__mobile_menu-in.isActive').classList.add('isSlided');
+            document.querySelector('.content__in.isActive').classList.add('isSlided');
         } else {
             console.log(`назад к ${target}`);
         }
-        document.querySelector('.header__mobile_menu-in.isActive').classList.remove('isActive');
+        document.querySelector('.content__in.isActive').classList.remove('isActive');
         document.querySelector(`[data-screen="${target}"]`).classList.add('isActive')
         document.querySelector(`[data-screen="${target}"]`).classList.remove('isSlided')
     }
